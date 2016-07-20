ff58fe985dc13f450dd869bde376013628e367fa
SanicCadir <ilyadray2011@gmail.com>
2016-07-20 14:28:52 +0300

    Update init.lua

--- lua/cyka_alert/init.lua
+++ lua/cyka_alert/init.lua
@@ -7,14 +7,14 @@ util.AddNetworkString("cyka_alert")
 resource.AddFile("sound/cyka_alert.mp3")
 
 hook.Add("PlayerInitialSpawn","cyka_alert",function(ply)
-	MsgC(Color(0,0,255,255),"[CYKA ALERT] PlayerInitialSpawn throwed")
 	if ply:IsSuperAdmin() == true then
 		message = {}
 		table.insert(message,Color(255,0,0,255))
 		table.insert(message,"[CYKA ALERT] ")
 		table.insert(message,Color(255,255,0,255))
 		table.insert(message,"Superadmin ")
-		table.insert(message,team.GetColor(ply:Team()))
+		--table.insert(message,team.GetColor(ply:Team())) there is no need in this because player's team color 
+		--will be yellow (or idk, try and tell me pls)
 		table.insert(message,ply:Name().." ")
 		table.insert(message,Color(255,255,0,255))
 		table.insert(message,"joined the server")
